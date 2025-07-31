b304c6d3758d758911ec4b672c039c9c57b8e29b
Adam Crosser <adam.crosser@praetorian.com>
2025-07-31 15:05:18 -0500

    Initial Commit

---
+++ pkg/renderer/renderer.go
@@ -0,0 +1,71 @@
+// Copyright 2025 Praetorian Security, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package renderer
+
+import (
+	"fmt"
+	"html/template"
+	"io/fs"
+	"net/http"
+
+	"github.com/praetorian-inc/OAuthSeeker/static"
+)
+
+type Renderer struct {
+	baseTemplate *template.Template
+	viewTemplate *template.Template
+}
+
+func NewRenderer(viewName string) (*Renderer, error) {
+	baseTemplateContent, err := fs.ReadFile(static.AdminPanelTemplates, "admin/templates/base.html")
+	if err != nil {
+		return nil, fmt.Errorf("error reading base template: %v", err)
+	}
+
+	baseTemplate, err := template.New("base").Parse(string(baseTemplateContent))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing base template: %v", err)
+	}
+
+	viewTemplatePath := fmt.Sprintf("admin/templates/%s.html", viewName)
+	viewTemplateContent, err := fs.ReadFile(static.AdminPanelTemplates, viewTemplatePath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading view template %s: %v", viewName, err)
+	}
+
+	viewTemplate, err := baseTemplate.Clone()
+	if err != nil {
+		return nil, fmt.Errorf("error cloning base template: %v", err)
+	}
+
+	_, err = viewTemplate.Parse(string(viewTemplateContent))
+	if err != nil {
+		return nil, fmt.Errorf("error parsing view template %s: %v", viewName, err)
+	}
+
+	return &Renderer{
+		baseTemplate: baseTemplate,
+		viewTemplate: viewTemplate,
+	}, nil
+}
+
+func (r *Renderer) Render(w http.ResponseWriter, data interface{}) error {
+	err := r.viewTemplate.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return fmt.Errorf("error rendering template: %v", err)
+	}
+	return nil
+}
---
+++ pkg/utils/cert.go
@@ -0,0 +1,72 @@
+// Copyright 2025 Praetorian Security, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"log"
+	"math/big"
+	"net"
+	"time"
+)
+
+func GenerateSelfSignedCert(domain string) (certPEM, keyPEM []byte) {
+	priv, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		log.Fatalf("Failed to generate private key: %v", err)
+	}
+
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		log.Fatalf("Failed to generate serial number: %v", err)
+	}
+
+	certTemplate := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Self-Signed"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().Add(365 * 24 * time.Hour), // 1 year validity
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	if ip := net.ParseIP(domain); ip != nil {
+		certTemplate.IPAddresses = []net.IP{ip}
+	} else {
+		certTemplate.DNSNames = []string{domain}
+	}
+
+	certDER, err := x509.CreateCertificate(rand.Reader, &certTemplate, &certTemplate, &priv.PublicKey, priv)
+	if err != nil {
+		log.Fatalf("Failed to create certificate: %v", err)
+	}
+
+	certPEM = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
+	keyDER, err := x509.MarshalECPrivateKey(priv)
+	if err != nil {
+		log.Fatalf("Failed to marshal private key: %v", err)
+	}
+	keyPEM = pem.EncodeToMemory(&pem.Block{Type: "EC PRIVATE KEY", Bytes: keyDER})
+
+	return certPEM, keyPEM
+}
